fac04884328496937396b3d5191b1fe5ee228c48
jpm8888 <jpm8888@gmail.com>
2018-09-17 18:21:45 +0530

    initial commit

---
+++ app/src/main/java/jpm/example/Launcher.java
@@ -0,0 +1,13 @@
+package jpm.example;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class Launcher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
